fdebfabcae9dd425b8f5eecd2e15fc2988ca8b9f
William Martin <williammartin1995@yahoo.com>
2017-08-16 10:28:00 +0200

    Finished model_saver.py - still requires testing online

--- model_tools/model_saver.py
+++ model_tools/model_saver.py
@@ -10,7 +10,7 @@ import time
 MODEL_DIR = '/fhgfs/users/wmartin/models/'    # Path to model directory
 MODEL_SUMMARY = 'model_summary.csv'           # CSV summary of models
 
-def save_model(model, nn_str, comment="", verbose=False):
+def save_model(model, nn_str, params, comment="", verbose=False):
     """
     Saves a Keras model to the directory of models, updates the CSV summary with the model parameters
 
@@ -18,6 +18,8 @@ def save_model(model, nn_str, comment="", verbose=False):
         model - the Keras model to be saved
         nn_str - the string specifying what type of model was trained (e.g. "cnn", "snn", etc.)
                  (only used for the filename)
+        params - the parameter dictionary for the model
+                 (for format see the Keras cnn.py or snn.py)
         comment - string to put in comments field in CSV (default: "")
         verbose - if True, prints when model is saved (default: False)
 
@@ -27,9 +29,22 @@ def save_model(model, nn_str, comment="", verbose=False):
     datetime = time.strftime("%Y%m%d_%H%M%S_")
     save_path = MODEL_DIR + datetime + nn_str + 'Keras.h5'
     model.save(save_path)
+    
     if verbose:
         print("Model saved to " + save_path)
 
     # Update CSV file
-    with open(MODEL_DIR + MODEL_SUMMARY, 'a') as csvfile:
-        
+    with open(MODEL_DIR + MODEL_SUMMARY, 'a+') as csvfile:
+        # Read file to extract headers
+        reader = csv.DictReader(csvfile)
+        fields = reader.fieldnames
+
+        # Add comment to parameter dictionary
+        params['comments'] = comment
+
+        # Write param dictionary to file
+        writer = csv.DictWriter(csvfile, headers)
+        writer.writerow(params)
+
+    if verbose:
+        print("Model summary updated")
