2edc4446dcedb15da41fb57b44c3052a832163d3
William Martin <williammartin1995@yahoo.com>
2017-08-23 12:48:14 +0200

    Allow amount of regularisation to be set for lstm

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -33,7 +33,8 @@ def main(
             'no_epochs': 1000,
             'steps_per_epoch': 100,
             'dp_prob': 0.5,
-            'batch_norm': False
+            'batch_norm': False,
+            'regularise': 0.0
         },
         no_threads=10,
         implementation=0,
@@ -58,7 +59,7 @@ def main(
             steps_per_epoch - the number of batches in each epoch
             dp_prob - the proportion of double pulse waveforms shown at train time (default = 0.5)
             batch_norm - unused
-            regularise - if true, uses L2 regularisation on the weights for each layer (default = False)
+            regularise - sets the amount of L2 regularisation for each layer (default = 0.0)
         no_threads - number of threads to use (default is 10, use -1 to set no limit)
         implementation - sets the implementation used by Keras for the LSTM layers (default = 0)
             0 - RNN uses fewer, larger, matrix products (good for CPU but uses more memory)
@@ -97,9 +98,7 @@ def main(
     model = Sequential()
 
     # Set up regulariser
-    regulariser = None
-    if params['regularise']:
-        regulariser = l2(0.01)
+    regulariser = l2(params['regularise'])
 
     # Reshape input to fit with LSTM
     model.add(Reshape((128, 1), input_shape = (128,)))
@@ -236,10 +235,10 @@ if __name__ == "__main__":
         )
 
     parser.add_argument(
-            '-r', '--regularise', 
-            help='uses regularisation on each layer',
-            action='store_true', dest='regularise', 
-            default=False
+            '-r', '--regularisation', 
+            help='sets amount of regularisation on each layer (default = 0.0)',
+            type=float, dest='regularise', 
+            default=0.0
         )
 
     parser.add_argument(
