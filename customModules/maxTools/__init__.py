9d87db73c96af4da5c535a6e0d77e2f5938927e2
William Martin <williammartin1995@yahoo.com>
2017-08-26 11:00:09 +0200

    Improve documentation to module maxTools
    This commit improves the documentation to any changes made in the
    module maxTools and enables the statement `from maxTools import *`.

---
+++ customModules/maxTools/__init__.py
@@ -0,0 +1,13 @@
+'''
+A package containing the tools written by Max Meier
+
+Contains:
+    sklearn_utils - a package with methods for calculating confusion 
+                    matrices on the waveform data
+    waveform_dataset - a package defining classes and methods necessary
+                       for loading and using the waveform data
+                       (this package contains some slight modifications 
+                       made by myself)
+'''
+
+__all__ = ["sklearn_utils", "waveform_dataset"]
