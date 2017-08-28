c51259f2cc442293f1113197857bec5bad3bffef
William Martin <williammartin1995@yahoo.com>
2017-08-28 17:09:11 +0200

    Improve documentation for myTools.metrics
    This commit improves documentation for myTools.metrics and makes stylistic
    changes to bring the code in line with the Python style guidelines.

--- customModules/myTools/metrics/sklearn.py
+++ customModules/myTools/metrics/sklearn.py
@@ -1,67 +1,101 @@
-#!/usr/bin/env python
-# Contains the metrics defined for use on np arrays
+'''
+A module containing metrics for evaluating model performance after training - 
+takes numpy arrays.
+
+'''
 from __future__ import print_function, absolute_import, division
 
 from sklearn.metrics import confusion_matrix, roc_auc_score
 
-from maxTools.sklearn_utils import weighted_unique_confusion_matrix, weighted_confusion_matrix
+from maxTools.sklearn_utils import \
+    weighted_unique_confusion_matrix, weighted_confusion_matrix
+
+# Most functions in this module are based off their unthresholded counterparts
+# in maxTools.waveform_dataset
 
 def threshold_confusion_matrix(y_true, y_pred, th=0.5):
-    """Confusion matrix with threshold in predictions
+    """
+    Computes confusion matrix with a threshold in predictions.
+
+    Takes numpy arrays
     
-    Inputs:
+    Arguments:
         y_true - labels
         y_pred - predictions
-        th     - probability threshold above which the signal class is
-                 considered to predict signal (default 0.5)
+        th - probability threshold above which the signal class is
+             considered to predict signal (default: 0.5)
     
-    Outputs:
-        A confusion matrix
+    Returns:
+        confusion_matrix - a numpy array containing the confusion matrix
+
     """
     # This statement flattens vectors from one-hot, thresholds predictions
     return confusion_matrix(y_true.nonzero()[1], y_pred[:, 1] > th)
 
 def threshold_weighted_confusion_matrix(y_true, y_pred, weights, th=0.5):
-    """Confusion matrix with threshold in predictions
-    
-    Inputs:
-        y_true - labels
-        y_pred - predictions
-        th     - probability threshold above which the signal class is
-                 considered to predict signal (default 0.5)
-    
-    Outputs:
-        A confusion matrix
     """
-    # This statement flattens vectors from one-hot, thresholds predictions
-    return weighted_confusion_matrix(y_true.nonzero()[1], y_pred[:, 1] > th, weights)
+    Computes a weighted confusion matrix with a threshold in predictions.
 
-def threshold_weighted_unique_confusion_matrix(y_true, y_pred, weights, ids, th=0.5):
-    """Confusion matrix with threshold in predictions
+    Takes numpy arrays
     
-    Inputs:
+    Arguments:
         y_true - labels
         y_pred - predictions
-        th     - probability threshold above which the signal class is
-                 considered to predict signal (default 0.5)
+        weights - weights for each waveform
+        th - probability threshold above which the signal class is
+             considered to predict signal (default: 0.5)
     
-    Outputs:
-        A confusion matrix
+    Returns:
+        confusion_matrix - a numpy array containing the confusion matrix
+
     """
     # This statement flattens vectors from one-hot, thresholds predictions
-    return weighted_unique_confusion_matrix(y_true.nonzero()[1], y_pred[:, 1] > th, weights, ids)
+    return weighted_confusion_matrix(y_true.nonzero()[1],
+                                         y_pred[:, 1] > th, weights)
+
+def threshold_weighted_unique_confusion_matrix(y_true, y_pred,
+                                               weights, ids, th=0.5):
+    """
+    Computes a weighted event-wise confusion matrix with a threshold in 
+    predictions.
+
+    Takes numpy arrays
+    
+    Arguments:
+        y_true - labels
+        y_pred - predictions
+        weights - weights for each waveform
+        ids - ids to correlate waveforms with events
+        th - probability threshold above which the signal class is
+             considered to predict signal (default: 0.5)
+    
+    Returns:
+        confusion_matrix - a numpy array containing the confusion matrix
+
+    """
+    # This statement flattens vectors from one-hot, thresholds predictions
+    return weighted_unique_confusion_matrix(y_true.nonzero()[1],
+                                            y_pred[:, 1] > th, weights, ids)
 
 def metric_summary(y_true, y_pred, th=0.5):
-    """Confusion matrix with threshold in predictions
+    """
+    Computes a summary of different metrics at a given threshold. The metrics
+    computed are accuracy, precision, recall and F1 score.
+
+    Takes numpy arrays
     
-    Inputs:
+    Arguments:
         y_true - labels
         y_pred - predictions
-        th     - probability threshold above which the signal class is
-                 considered to predict signal (default 0.5)
+        th - probability threshold above which the signal class is
+             considered to predict signal (default: 0.5)
     
-    Outputs:
-        accuracy, precision, recall, f1
+    Returns:
+        accuracy - a float
+        precision - a float
+        recall - a float
+        f1 - a float
+
     """
     [[tn, fp], [fn, tp]] = threshold_confusion_matrix(y_true, y_pred, th)
 
@@ -73,7 +107,21 @@ def metric_summary(y_true, y_pred, th=0.5):
     return accuracy, precision, recall, f1
 
 def print_metric_results(y_true, y_pred, weights, ids, th=0.5):
-    """Prints a summary output of different metrics.
+    """
+    Prints a summary output of different metrics.
+
+    Takes numpy arrays
+    
+    Arguments:
+        y_true - labels
+        y_pred - predictions
+        weights - weights for each waveform
+        ids - ids to correlate waveforms with events
+        th - probability threshold above which the signal class is
+             considered to predict signal (default: 0.5)
+    
+    No returns
+    
     """
     accuracy, precision, recall, f1 = metric_summary(y_true, y_pred, th)
 
