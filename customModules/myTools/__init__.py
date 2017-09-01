ec99e86dee0ee83092713683298231904364b49b
William Martin <williammartin1995@yahoo.com>
2017-09-01 11:01:28 +0200

    Add documentation for the seed_setter code

--- customModules/myTools/__init__.py
+++ customModules/myTools/__init__.py
@@ -3,11 +3,12 @@ A package containing the tools I have written to work with the waveform data
 and Keras
 
 Contains:
-    metrics - contains metrics for Keras and post-training evaluation
-    model_tools - contains tools for saving and loading models
-    waveform_tools - contains tools for loading the data and feeding batches to
-                     Keras
+    metrics - package of metrics for Keras and post-training evaluation
+    model_tools - package of tools for saving and loading models
+    waveform_tools - package of tools for loading the data and feeding batches
+                     to Keras
+    seed_setter - module for setting Keras random seed
 
 '''
 
-__all__ = ['metrics', 'model_tools', 'waveform_tools']
+__all__ = ['metrics', 'model_tools', 'waveform_tools', 'seed_setter']
--- customModules/myTools/seed_setter.py
+++ customModules/myTools/seed_setter.py
@@ -1,5 +1,5 @@
 '''
-Module containing a function that sets all the necessary seeds for Keras
+Module for setting all the necessary seeds for Keras
 
 '''
 import os
@@ -8,12 +8,30 @@ import random as rn
 import tensorflow as tf
 
 def set_seed(s):
+    '''
+    Sets all random seeds used by Keras
+
+    Arguments:
+        s - an integer, the seed
+
+    No returns
+
+    '''
     os.environ['PYTHONHASHSEED'] = str(s)
     np.random.seed(s)
     rn.seed(s)
     tf.set_random_seed(s)
 
 def check_seed_set():
+    '''
+    Checks if environment variable RANDOM_SEED is set, and if so, sets the 
+    random seeds. 
+
+    No arguments
+
+    No returns
+    
+    '''
     # If environment variable RANDOM_SEED is set, set seed
     RANDOM_SEED = os.environ.get('RANDOM_SEED')
 
