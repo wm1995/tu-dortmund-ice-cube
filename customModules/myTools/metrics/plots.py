5ae56a4fc190b00e8510b52340dcce93266f38e2
William Martin <williammartin1995@yahoo.com>
2017-09-06 10:45:31 +0200

    Improve the purity-efficiency plot
    This commit improves the purity-efficiency plot-making function.

--- customModules/myTools/metrics/plots.py
+++ customModules/myTools/metrics/plots.py
@@ -4,16 +4,13 @@ Module for generating plots to assess model performance
 '''
 import numpy as np
 
-# Set backend
 import matplotlib
-matplotlib.use('gtk')
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
 
 from sklearn.metrics import precision_recall_curve
 
-def purity_efficiency_plot(y_true, y_pred, inset=True):
+def purity_efficiency_plot(y_true, y_pred, inset=True, savepath=None):
     # Purity = precision
     # Efficiency = recall
     purity, efficiency, thresholds = precision_recall_curve(y_true[:, 1], y_pred[:, 1])
@@ -23,26 +20,40 @@ def purity_efficiency_plot(y_true, y_pred, inset=True):
     purity = purity[:-1]
     efficiency = efficiency[:-1]
 
+    # Set larger font for legibility
+    matplotlib.rcParams.update({'font.size': 14})
+
     # Code adapted from http://matplotlib.org/1.3.1/mpl_toolkits/axes_grid/examples/inset_locator_demo.py
     fig, ax = plt.subplots()
 
+    # Plot purity and efficiency
     ax.plot(thresholds, purity, label="Purity")
     ax.plot(thresholds, efficiency, label="Efficiency")
 
-    axins = zoomed_inset_axes(ax, zoom=5, loc=4, borderpad=5)
+    # If necessary, make an inset plot
+    if inset:
+        # Use a zoom of five, place in lower left corner, pad border by 3
+        axins = zoomed_inset_axes(ax, zoom=5, loc=4, borderpad=3)
 
-    axins.plot(purity, thresholds)
-    axins.plot(efficiency, thresholds)
+        axins.plot(purity, thresholds)
+        axins.plot(efficiency, thresholds)
 
-    axins.set_xlim(0.9, 1)
-    axins.set_ylim(0.9, 1)
+        # Set limits for inset plot
+        axins.set_xlim(0.9, 1)
+        axins.set_ylim(0.9, 1)
 
-    ax.xlabel("Confidence Cut")
-    ax.ylabel("Quality Parameter")
+        # Mark inset box
+        mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
-    mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+    ax.set_xlabel("Confidence Cut")
+    ax.set_ylabel("Quality Parameter")
+    
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    
+    ax.legend(bbox_to_anchor=(0.5, 1), loc="lower center", ncol=2, frameon=False)
 
-    ax.legend(loc=2)
-
-    plt.draw()
-    plt.show()
+    if savepath == None:
+        plt.show()
+    else:
+        plt.savefig(savepath)
