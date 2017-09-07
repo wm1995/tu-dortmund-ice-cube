858836fbfd27dad0eb8ccf810b802ac30e695c4b
William Martin <williammartin1995@yahoo.com>
2017-09-07 13:41:12 +0200

    Comment plotting code
    This commit adds comments to the plotting code and makes a minor
    alteration that improves the readability of the rate_plot method.

--- customModules/myTools/metrics/plots.py
+++ customModules/myTools/metrics/plots.py
@@ -44,9 +44,9 @@ def purity_efficiency_plot(y_true, y_pred, inset=True, savepath=None):
         # Set limits for inset plot
         axins.set_xlim(0.9, 1)
         axins.set_ylim(0.9, 1)
-	
-	# Set ticks for inset plot
-	axins.set_xticks([0.9, 0.95, 1.0])
+
+        # Set ticks for inset plot
+        axins.set_xticks([0.9, 0.95, 1.0])
         axins.set_yticks([0.9, 0.95, 1.0])
 
         # Mark inset box
@@ -54,58 +54,74 @@ def purity_efficiency_plot(y_true, y_pred, inset=True, savepath=None):
 
     ax.set_xlabel("Confidence Cut")
     ax.set_ylabel("Quality Parameter")
-    
+
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
-    
+
     ax.legend(bbox_to_anchor=(0.5, 1), loc="lower center", ncol=2, frameon=False)
 
-    if savepath == None:
+    if savepath is None:
         plt.show()
     else:
         plt.savefig(savepath)
 
 def rate_plot(y_true, y_pred, weights, bin_size=0.02, combine_nu_tau_cc=False, savepath=None):
-    bins = np.arange(0, 1.0, bin_size)
-    counts = np.zeros((y_true.shape[1], len(bins) + 1))
-    inds = np.digitize(y_pred[:, 1], bins)
+    # Set up bins and counts
+    # Need 0 to 1 inclusive
+    bins = np.arange(0, 1.0 + 1e-8, bin_size)
+    counts = np.zeros((y_true.shape[1], len(bins)))
+
+    # Get bin indices
+    inds = np.digitize(y_pred[:, 1], bins, right=True)
+
+    # Convert labels to array of masks
+    # i.e. if 5 labels, label_mask[1] is a mask that selects only events label 2
     label_mask = y_true.astype(bool)
 
-    # Set larger font for legibility
-    matplotlib.rcParams.update({'font.size': FONT_SIZE})
-
+    # Sum weights for each bin
     for i in range(len(inds)):
         counts[label_mask[i, :], inds[i]] += weights[i]
 
+    # Set labels for each bin
     labels = {'0':r'Cascade-like background',
               '1':r'Double-pulse events',
               '2':r'Other $\nu_\tau$ CC events',
               '3':r'$\nu_\mu$ CC events',
               '4':r'Atmospheric muons'}
 
+    # If combining nu_tau_cc events, change label
     if combine_nu_tau_cc:
-        labels['1'] =r'$\nu_\tau$ CC events'
+        labels['1'] = r'$\nu_\tau$ CC events'
 
-    bin_range = np.arange(0, 1 + 1e-8, bin_size)
+    # Set larger font for legibility
+    matplotlib.rcParams.update({'font.size': FONT_SIZE})
 
+    # Prepare plots
     fig, ax = plt.subplots()
 
+    # With FONT_SIZE = 14, need to shrink plot
     box = ax.get_position()
+
+    # Set offsets for shrinking
     x_offset = 0.03
     if counts.shape[0] == 2:
         y_offset = 0
     elif counts.shape[0] == 5:
         y_offset = 0.05
 
+    # Shrink plot
     ax.set_position([box.x0 + box.width*x_offset, box.y0, box.width * (1 - x_offset), box.height * (1 - y_offset)])
 
-    for i in range(counts.shape[0]):    
+    # Plot events for each label
+    for i in range(counts.shape[0]):
         if combine_nu_tau_cc:
+            # If combining events, combine under label 1
             if i == 1 and counts.shape[0] > 2:
                 counts[1] += counts[2]
+            # This makes label 2 redundant, so we can skip it
             elif i == 2:
                 continue
-        ax.step(bin_range, counts[i], label=labels[str(i)])
+        ax.step(bins, counts[i], label=labels[str(i)])
 
     ax.set_xlabel("Confidence")
     ax.set_ylabel("Waveforms per year")
@@ -113,7 +129,7 @@ def rate_plot(y_true, y_pred, weights, bin_size=0.02, combine_nu_tau_cc=False, s
     ax.set_yscale('log')
     ax.legend(bbox_to_anchor=(0.5 - x_offset, 1), loc="lower center", ncol=2, frameon=False)
 
-    if savepath == None:
+    if savepath is None:
         plt.show()
     else:
         plt.savefig(savepath)
