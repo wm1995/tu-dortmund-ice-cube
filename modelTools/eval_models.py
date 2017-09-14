33ca5fb09bf9671d312904b599548eadb9502827
William Martin <williammartin1995@yahoo.com>
2017-09-14 13:09:50 +0200

    Add a confidence cut survival rate plot

--- modelTools/eval_models.py
+++ modelTools/eval_models.py
@@ -7,7 +7,7 @@ from myTools.train_tools.resource_limiter import limit_resources
 from myTools.waveform_tools.data_loader import load_eval_data
 from myTools.model_tools.model_loader import load_model
 from myTools.metrics.sklearn import print_metric_results
-from myTools.metrics.plots import purity_efficiency_plot, rate_plot, event_rate_plot
+from myTools.metrics.plots import purity_efficiency_plot, rate_plot, event_rate_plot, cut_survival_plot
 
 def main(
         model_list,
@@ -48,6 +48,7 @@ def main(
         test_preds = model.predict(dataset.waveforms, verbose=int(verbose))
 
         training_mask = np.logical_or(dataset.labels[:, 0] == 1, dataset.labels[:, 1] == 1)
+        tau_mask = np.logical_or(training_mask, dataset.labels[:, 1] == 1)
 
         print()
         print_metric_results(dataset.labels[training_mask, 0:2], test_preds[training_mask], weights[training_mask], dataset.ids[training_mask], th=0.5)
@@ -56,9 +57,14 @@ def main(
 
         purity_efficiency_plot(dataset.labels[training_mask, 0:2], test_preds[training_mask], savepath="plots/" + model_name + "_pe_plot.pdf")
         rate_plot(dataset.labels[training_mask, 0:2], test_preds[training_mask], weights[training_mask], savepath="plots/" + model_name + "_train_rate_plot.pdf")
+        rate_plot(dataset.labels[tau_mask, 0:3], test_preds[tau_mask], weights[tau_mask], savepath="plots/" + model_name + "_tau_rate_plot.pdf")
         rate_plot(dataset.labels, test_preds, weights, combine_nu_tau_cc=True, savepath="plots/" + model_name + "_rate_plot.pdf")
         event_rate_plot(dataset.labels[training_mask, 0:2], test_preds[training_mask], weights[training_mask], dataset.ids[training_mask], savepath="plots/" + model_name + "_train_event_rate_plot.pdf")
+        event_rate_plot(dataset.labels[tau_mask, 0:3], test_preds[tau_mask], weights[tau_mask], dataset.ids[tau_mask], savepath="plots/" + model_name + "_tau_event_rate_plot.pdf")
         event_rate_plot(dataset.labels, test_preds, weights, dataset.ids, combine_nu_tau_cc=True, savepath="plots/" + model_name + "_event_rate_plot.pdf")
+        cut_survival_plot(dataset.labels[training_mask, 0:2], test_preds[training_mask], weights[training_mask], dataset.ids[training_mask], savepath="plots/" + model_name + "_train_cut_survival_plot.pdf")
+        cut_survival_plot(dataset.labels[tau_mask, 0:3], test_preds[tau_mask], weights[tau_mask], dataset.ids[tau_mask], savepath="plots/" + model_name + "_tau_cut_survival_plot.pdf")
+        cut_survival_plot(dataset.labels, test_preds, weights, dataset.ids, combine_nu_tau_cc=True, savepath="plots/" + model_name + "_cut_survival_plot.pdf")
 
 if __name__ == "__main__":
     # Initialise the arg parser
